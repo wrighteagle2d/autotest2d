4095feb9f36ceb16ba02ac0db9d2b4ec3c994e0b
baj <>
2010-03-10 04:14:33 +0000

    add log support

--- clear.sh
+++ clear.sh
@@ -2,4 +2,5 @@
 
 rm -f result
 rm -f result_*
+rm -fr log_*
 rm -f plot
--- match.sh
+++ match.sh
@@ -9,15 +9,17 @@ if [ ! -z $1 ]; then
 	SERVER_HOST=$1
 fi
 
-RSTLOG="result_${SERVER_HOST}"
+RESULT="result_${SERVER_HOST}"
+LOGDIR="log_${SERVER_HOST}"
 
 START_LEFT="-server::team_l_start=\"./start_left ${SERVER_HOST}\""
 START_RIGHT="-server::team_r_start=\"./start_right ${SERVER_HOST}\""
-GAME_LOG_DIR="-server::game_log_dir=\"./log/\""
-TEXT_LOG_DIR="-server::text_log_dir=\"./log/\""
-ADDITION_OPTS="-server::nr_normal_halfs=2 -server::nr_extra_halfs=0 -server::penalty_shoot_outs=false -server::auto_mode=on -server::game_logging=false -server::text_logging=false -server::host=\"${SERVER_HOST}\""
+GAME_LOG_DIR="-server::game_log_dir=\"./${LOGDIR}/\""
+TEXT_LOG_DIR="-server::text_log_dir=\"./${LOGDIR}/\""
+ADDITION_OPTS="-server::nr_normal_halfs=2 -server::nr_extra_halfs=0 -server::penalty_shoot_outs=false -server::auto_mode=on -server::game_logging=true -server::text_logging=false -server::host=\"${SERVER_HOST}\""
 
-exec > $RSTLOG
+mkdir $LOGDIR
+exec > $RESULT
 
 for i in $RANGE; do
 	$SERVER $START_LEFT $START_RIGHT $GAME_LOG_DIR $TEXT_LOG_DIR $ADDITION_OPTS
