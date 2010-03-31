ac5357499db6244941b82b729e3382e74aed2f4f
baj <>
2010-03-31 18:59:19 +0000

    fix

--- analyze.sh
+++ analyze.sh
@@ -6,6 +6,11 @@ AWKFILE="../analyze.awk"
 PLOTDATA="./plot"
 GNUPLOT="../plot.gnuplot"
 
+if [ ! -z $1 ]; then
+    RESULT_DIR=$1
+fi
+
+
 cd $RESULT_DIR 2>/dev/null || exit
 echo "#count win_rate expected_win_rate" > $PLOTDATA
 cat `ls -1 | grep '[0-9]\+' | sort -n` | grep 'Score' | awk -f $AWKFILE >> $PLOTDATA
--- result.sh
+++ result.sh
@@ -3,6 +3,10 @@
 RESULT_DIR="result.d"
 AWKFILE="../statistic.awk"
 
+if [ ! -z $1 ]; then
+    RESULT_DIR=$1
+fi
+
 cd $RESULT_DIR 2>/dev/null || exit
 RESULT_LIST=`ls -1 | grep '[0-9]\+' | sort -n`
 cat `echo $RESULT_LIST | awk '{print $1}'` | grep '\<vs\>' | sed -e 's/\t//g'
