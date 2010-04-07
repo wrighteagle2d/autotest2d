cf327128d6c7b03ca64592be7d8b06d269d2667c
baj <>
2010-04-07 03:30:43 +0000

    fix

--- analyze.sh
+++ analyze.sh
@@ -16,7 +16,7 @@ cd $RESULT_DIR 2>/dev/null || exit
 echo "#count win_rate expected_win_rate" > $PLOTDATA
 cat `ls -1 | grep '[0-9]\+' | sort -n` | grep 'Score' | awk -f $AWKFILE >> $PLOTDATA
 
-$GNUPLOT
+$GNUPLOT 2>/dev/null
 
 EOG=`which eog`
 
