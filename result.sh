346f39a40f9d4a5103d93aadb31e355edd6f1274
baj <>
2010-04-08 22:29:51 +0000

    fix

--- result.sh
+++ result.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 RESULT_DIR="result.d"
-AWKFILE="../statistic.awk"
+PARSE="../parse.awk"
+PROCESS="../process.awk"
 
 if [ ! -z $1 ]; then
     RESULT_DIR=$1
@@ -9,8 +10,14 @@ fi
 
 cd $RESULT_DIR 2>/dev/null || exit
 
-RESULT_LIST=`ls -1 | grep '[0-9]\+' | sort -n | ../filter.sh`
+RESULT_LIST=`ls -1 | grep '[0-9]\+' | sort -n`
+
+parseall() {
+    for i in $RESULT_LIST; do
+        cat $i | awk -f $PARSE
+    done 
+}
 
 cat `echo $RESULT_LIST | awk '{print $1}'` | grep '\<vs\>' | sed -e 's/\t//g'
 echo
-cat $RESULT_LIST | grep 'Score' | awk -f $AWKFILE
+parseall | ../process.py
