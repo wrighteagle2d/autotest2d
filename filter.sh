8d8fb20d51b8b673782fc06d7f73e673016ea29c
baj <>
2010-04-08 07:01:07 +0000

    adjust

--- filter.sh
+++ filter.sh
@@ -3,16 +3,18 @@
 FILTER="false"
 #FILTER="true"
 
-while read FILE; do
-    if [ ! $FILTER = "true" ]; then
-        echo $FILE
-        continue
-    fi
 
-    WAITING_LN=`cat $FILE | grep -n 'Waiting after end of match' | awk -F: '{print $1}'`
-    DISCONNECTED_LN=`cat $FILE | grep -n 'A player disconnected' | awk -F: '{print $1}' | head -1`
-
-    if [ $DISCONNECTED_LN -gt $WAITING_LN ]; then
+if [ ! $FILTER = "true" ]; then
+    while read FILE; do
         echo $FILE
-    fi
-done
+    done
+else
+    while read FILE; do
+        WAITING_LN=`cat $FILE | grep -n 'Waiting after end of match' | awk -F: '{print $1}'`
+        DISCONNECTED_LN=`cat $FILE | grep -n 'A player disconnected' | awk -F: '{print $1}' | head -1`
+
+        if [ $DISCONNECTED_LN -gt $WAITING_LN ]; then
+            echo $FILE
+        fi
+    done
+fi
