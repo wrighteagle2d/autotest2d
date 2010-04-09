b3f110854b3378ba373cd502c917a062664215ec
baj <>
2010-04-09 00:16:25 +0000

    fix

--- result.sh
+++ result.sh
@@ -3,6 +3,7 @@
 RESULT_DIR="result.d"
 PARSE="../parse.awk"
 PROCESS="../process.py"
+MARK="/tmp/result_running"
 
 if [ ! -z $1 ]; then
     RESULT_DIR=$1
@@ -10,14 +11,36 @@ fi
 
 cd $RESULT_DIR 2>/dev/null || exit
 
+RESULT=`mktemp`
 RESULT_LIST=`ls -1 | grep '[0-9]\+' | sort -n`
 
 parseall() {
     for i in $RESULT_LIST; do
         cat $i | awk -f $PARSE
-    done 
+    done
 }
 
-cat `echo $RESULT_LIST | awk '{print $1}'` | grep '\<vs\>' | sed -e 's/\t//g'
-echo
-parseall | python $PROCESS
+spinner(){
+    local DELAY=0.05
+    sleep $DELAY
+    while [ ! -f $MARK ]; do
+        sleep $DELAY
+    done
+
+    while [ -f $MARK ]; do
+        echo -n '/' ; sleep $DELAY
+        echo -n '-' ; sleep $DELAY
+        echo -n '\' ; sleep $DELAY
+        echo -n '|' ; sleep $DELAY
+    done
+    echo -n ''
+}
+
+rm -f $MARK
+spinner &
+touch $MARK
+cat `echo $RESULT_LIST | awk '{print $1}'` | grep '\<vs\>' | sed -e 's/\t//g' >$RESULT
+echo >$RESULT
+parseall | python $PROCESS >$RESULT
+rm -f $MARK
+cat $RESULT
